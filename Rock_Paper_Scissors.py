d9f080b542a1eb5d39d28c5b678a25442b4c9a53
Zeynab Sarafraz <z.srfz.yz@gmail.com>
2023-07-13 21:00:05 +0330

    Update and rename

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,32 +1,32 @@
 import random
 
-i=5
+i = 5
 while i:
    
-    print('choose \n1-sang⚫\n2-kaqaz📄\n3-qeichi🔪')
-    user=int(input())
-    comp=random.randint(1,3)
-    if user==1 :
-        if comp==1 :
+    print('choose \n1-Rock🤜🏼\n2-Paper✋🏼\n3-Scissors✌🏼')
+    user = int(input())
+    comp = random.randint(1,3)
+    if user == 1 :
+        if comp == 1 :
             print('No One!!\n')
-        elif comp==2 :       
+        elif comp == 2 :       
             print('Computer Win!\n')
         else :
             print('User Win!\n') 
-    elif user==2:
-        if comp==1:           
+    elif user == 2:
+        if comp == 1:           
             print('User Win!\n')
-        elif comp==2 :    
+        elif comp == 2 :    
             print('No One!!\n')
         else :
             print('Computer Win!\n')
-    elif user==3 :
-        if comp==1 :  
+    elif user == 3 :
+        if comp == 1 :  
             print('Computer Win!\n')
-        elif comp==2 :
+        elif comp == 2 :
             print('User Win!\n')
         else :    
             print('No One!!\n')
     else :
         print('Invalid!\n')   
-    i-=1         
+    i -= 1         
