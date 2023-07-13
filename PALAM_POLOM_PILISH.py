195391a9747eeb74a0386ecd782a7eb3ac950ae8
Zeynab Sarafraz <z.srfz.yz@gmail.com>
2023-07-13 21:06:02 +0330

    Update and rename

--- PALAM_POLOM_PILISH.py
+++ PALAM_POLOM_PILISH.py
@@ -5,14 +5,14 @@ while True:
     comp1 = random.randint(1, 2)
     comp2 = random.randint(1, 2)
     user = int(input('choose :'))
-    if comp1==comp2 and comp1!=user :
+    if comp1 == comp2 and comp1!=user :
         print('user lose!!')
-    elif comp1==user and comp1!=comp2 :
+    elif comp1 == user and comp1!=comp2 :
         print('computer 2 lose :)')
-    elif comp2==user and comp2!=comp1 :
+    elif comp2 == user and comp2!=comp1 :
         print('computer 1 lose :)')
-    elif comp1==comp2==user:
+    elif comp1 == comp2 == user:
         print('all the gamer same\ntry again!')
-    elif user!=1 or user!=2 :
+    elif user != 1 or user != 2 :
         print('Only numbers 1 or 2')
         sys.exit(0)
